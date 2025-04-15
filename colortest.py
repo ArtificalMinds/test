107ecdfde530727834845faee9d43cf0154a1da8
Aman Kumar <toartificialminds@gmail.com>
2025-04-15 11:47:19 +0530

    added third suggestion

--- colortest.py
+++ colortest.py
@@ -1,6 +1,7 @@
 color= input("Which color you like?")
 if color == "red":
-    print("Red color is sign of danager")
-    print("Other color like blue suits better to you")
+    print("Suggestion 1: Red color is sign of danager")
+    print("Suggestion 2: Other color like blue suits better to you")
+    print("Suggestion 3: or may be from yellow or purple")
 else:
     print("Good you like color other than Red")
