536c4daa9226f3a0ea7881b7c9d2cfa145f644b3
Aman Kumar <toartificialminds@gmail.com>
2025-04-15 11:07:06 +0530

    Revert "added more remarks for user"
    This reverts commit 080d3cf9b441d70df016aaaffdbf944a738dfe93.

--- colortest.py
+++ colortest.py
@@ -2,6 +2,5 @@ color= input("Which color you like?")
 if color == "red":
     print("Red color is sign of danager")
     print("Why always Red, choose another color out of so many")
-    print("Otherwise also this color is deep red so better to choose other one that goes with entire theme")
 else:
     print("Good you like color other than Red")
