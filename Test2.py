865cbb69674990f55fb9331717822ea6abc0e1bf
AKJ1975 <foramanjindal@gmail.com>
2025-04-15 17:14:30 +0530

    added a flag to if condition

--- Test2.py
+++ Test2.py
@@ -1,5 +1,5 @@
 cuisinType=input("Which Cuisin you prefer? ")
-if cuisinType == "Indian" :
+if cuisinType == "Asian" and "Indian" :
     print("Response 1: Indian")
     print("Response 2: Italian")
     print("Response3: Maxican")
