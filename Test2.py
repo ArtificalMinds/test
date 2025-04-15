c76ad0dd5d82ca69a9307e8cd96a7b21b9ac3e2e
Aman Kumar <toartificialminds@gmail.com>
2025-04-15 12:13:08 +0530

    added Response3

--- Test2.py
+++ Test2.py
@@ -2,5 +2,6 @@ cuisinType=input("Which Cuisin you prefer? ")
 if cuisinType == "Indian" :
     print("Response 1: Indian")
     print("Response 2: Italian")
+    print("Response3: Maxican")
 else:
     print("Looks like you are not Indian")
