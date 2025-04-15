9909272cb365bd845662111dee8a5d5bd868f10c
Aman Kumar <toartificialminds@gmail.com>
2025-04-15 12:16:23 +0530

    reverted response4

--- Test2.py
+++ Test2.py
@@ -3,5 +3,6 @@ if cuisinType == "Indian" :
     print("Response 1: Indian")
     print("Response 2: Italian")
     print("Response3: Maxican")
+    print("Response 4: Thai")
 else:
     print("Looks like you are not Indian")
