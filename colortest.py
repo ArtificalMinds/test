24f96a9c217abd0e9ce956a6433811d48b2181af
Aman Kumar <toartificialminds@gmail.com>
2025-04-15 11:16:59 +0530

    Deleted extra remarks for user

--- colortest.py
+++ colortest.py
@@ -1,6 +1,5 @@
 color= input("Which color you like?")
 if color == "red":
     print("Red color is sign of danager")
-    print("Why always Red, choose another color out of so many")
 else:
     print("Good you like color other than Red")
