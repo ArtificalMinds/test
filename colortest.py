6d6c9bd295c2453cc6cce46d3ae6065db8d9449f
Aman Kumar <toartificialminds@gmail.com>
2025-04-15 09:34:31 +0530

    added user input for color

--- colortest.py
+++ colortest.py
@@ -1,1 +1,5 @@
-print("This is Red Color")
+color= input("Which color you like?")
+if color == "red":
+    print("Red color is sign of danager")
+else:
+    print("Good you like color other than Red")
